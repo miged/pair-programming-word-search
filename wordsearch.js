d844047388dc5cd6e6f41eb667156e39060e945d
Miguel <mige.d@outlook.com>
2021-11-03 22:18:39 -0400

    fix word search w/ tests

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,28 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map((ls) => ls.join(''));
+  const verticalJoin = transpose(letters).map((ls) => ls.join(''));
+  const joins = horizontalJoin.concat(verticalJoin);
 
-module.exports = wordSearch
+  for (let l of joins) {
+    if (l.includes(word)) return true;
+  }
+
+  return false;
+};
+
+const transpose = function(matrix) {
+  let array = [];
+  if (matrix.length === 0) return [];
+
+  matrix[0].forEach(() => array.push([]));
+
+  matrix.forEach((_, row) => {
+    matrix[row].forEach((_, col) =>
+      array[col][row] = matrix[row][col]
+    );
+  });
+
+  return array;
+};
+
+module.exports = wordSearch;
